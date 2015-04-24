341c55c7a2776c3768963b560e8f75829cd9173b
lq <lq@le1.ca>
2015-04-24 15:40:34 -0600

    move join handling outside api

--- main.py
+++ main.py
@@ -6,10 +6,14 @@
 import time
 from rtrans import rt
 
-sim_loss=0.6
+sim_loss=0
         
 def cb(s, t, p):
-    print("Package from %04x of type %02x:\n%s\n" % (s, t, p))
+    if t == rt.ptype['DATA']:
+        print("Data from %04x:\n%s\n" % (s, p))
+    elif t == rt.ptype['JOIN']:
+        print("Join from %04x." % (s))
+        transport.poll(s)
 
 transport = rt("/dev/ttyUSB1", 9600, '\x18\x4c', cb, sim_loss)
 transport.probe()
