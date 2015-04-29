d90022d8032eb148ebf6b97aebc248497a9c8997
lq <lq@le1.ca>
2015-04-29 14:59:36 -0600

    use application layer protocol

--- main.py
+++ main.py
@@ -5,16 +5,20 @@
 
 import time
 from rtrans import rt
+from rapp import rapp_pkt
 
 sim_loss=0
         
 def cb(s, t, p):
     if t == rt.ptype['DATA']:
-        print("Data from %04x:\n%s\n" % (s, p))
+        print("Data from %04x:" % s)
+        data = rapp_pkt(p)
+        for v in data.parsed:
+            print("time=%lu, voltage=%u, current=%u, temp=%d" % (v['timesta'], v['voltage'], v['current'], v['tempera']))
     elif t == rt.ptype['JOIN']:
         print("Join from %04x." % (s))
         transport.poll(s)
 
-transport = rt("/dev/ttyUSB1", 9600, '\x18\x4c', cb, sim_loss)
+transport = rt("/dev/ttyUSB1", 9600, '\x18\x4c', cb, sim_loss, 0.5)
 transport.probe()
 transport.wait()
