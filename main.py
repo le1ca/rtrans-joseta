a7b570f58f3ec838d45a51cc47a6228e02f6fd7c
Travis Mick <lq@le1.ca>
2015-08-06 17:47:16 -0600

    handle empty data and add comment to prevent future pain

--- main.py
+++ main.py
@@ -12,13 +12,23 @@ sim_loss=0
 def cb(s, t, p):
     if t == rt.ptype['DATA']:
         print("Data from %04x:" % s)
-        data = rapp_pkt(p)
-        for v in data.parsed:
-            print("time=%lu, voltage=%u, current=%u, temp=%d" % (v['timesta'], v['voltage'], v['current'], v['tempera']))
+        if len(p) > 0:
+                data = rapp_pkt(p)
+                for v in data.parsed:
+                    print("time=%lu, voltage=%u, current=%u, temp=%d" % (v['timesta'], v['voltage'], v['current'], v['tempera']))
+        else:
+                print("discarding 0-length packet")
     elif t == rt.ptype['JOIN']:
         print("Join from %04x." % (s))
         transport.poll(s)
 
-transport = rt("/dev/ttyUSB1", 9600, '\x18\x4c', cb, sim_loss, 0.5)
+# NOTE TO SELF:
+# =============================================================================
+# CONFIGURE THE XBEE WITH ATAP1 AND ATMM2 OR YOU WILL WASTE TWO DAYS DEBUGGING
+# SET ATMY TO THE LOW BYTE OF ATDL THEN COPY IT BELOW
+# THE TWO-BYTE ADDRESS HERE IS WITH THE REVERSE ENDIANNESS OF THE XBEE ITSELF
+# OR JUST IMPLEMENT A CONFIGURATION ROUTINE YOU LAZY BASTARD
+
+transport = rt("/dev/ttyUSB1", 9600, '\x1b\x63', cb, sim_loss, 0.5)
 transport.probe()
 transport.wait()
